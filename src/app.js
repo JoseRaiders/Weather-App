96c113547bf4bc072e0d9053eb3b094e5c12ec08
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2021-08-21 13:15:08 -0700

    Added the relevant weather icon from the API
    icon obtained from openweathermap.org

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ function formatDate(timestamp) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
-  }
+    }
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
-  }
+    }
 
   let days = [
     "Sunday",
@@ -22,13 +22,20 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
-}
+// function formatDay(timestamp) {
+//   let date = new Date(timestamp * 1000);
+//   let day = date.getDay();
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+// ];
+//   return days[day];
+// }
 
 function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
@@ -37,12 +44,15 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     cityElement.innerHTML = response.data.name.toUpperCase();
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let city = "Los Angeles";
